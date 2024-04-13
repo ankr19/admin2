009a656776a971a72ccb85e556d69f084f054ea5
anand kumar <ankm335@gmail.com>
2024-04-13 23:40:42 +0530

    ui: added component

---
+++ common/Alert.js
@@ -0,0 +1,29 @@
+import { toast, Bounce } from "react-toastify";
+
+export async function Alert(val) {
+  if (val) {
+    toast("Main has sent", {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      transition: Bounce,
+    });
+  } else {
+    toast.warning("Mail has not been sent", {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      transition: Bounce,
+    });
+  }
+}
